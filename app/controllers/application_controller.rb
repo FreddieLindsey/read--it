fb0dfdbab4c1139112d5de08e03c507ce5da3340
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-07-14 17:21:35 +0100

    Added password to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
   end
 
 end
