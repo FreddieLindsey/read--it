9e42bf9982eb616ccb1dce97c5e56b806c4cf6b9
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-07-14 17:27:02 +0100

    Added username check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # Devise generated user model
 class User < ActiveRecord::Base
 
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :email, presence: true,
             format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
                       message: 'Not a valid email!' }
